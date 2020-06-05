2b8e5529066f84fed35a74b64641339b278d7d6a
Guilherme Cardoso <guilhermemunizcardoso@gmail.com>
2020-06-05 17:33:27 -0300

    Changed the sort method approach

--- src/utils/repo.js
+++ src/utils/repo.js
@@ -1,21 +1,14 @@
 export const orderBy = (list, sort = 'asc') => {
-    let array = [...list];
+	let array = [...list];
+	const sortMethod = { asc: 1, desc: -1 };
 
-    if(sort === 'asc') {
-        array.sort((a, b) => {
-            if (a.stargazers_count < b.stargazers_count) return -1;
-            if (a.stargazers_count > b.stargazers_count) return 1;
-            return 0;
-        });
+	array.sort((a, b) => {
+		if (a.stargazers_count < b.stargazers_count)
+			return -1 * sortMethod[sort];
+		if (a.stargazers_count > b.stargazers_count)
+			return 1 * sortMethod[sort];
+		return 0;
+	});
 
-        return array;
-    }
-
-    array.sort((a, b) => {
-        if (a.stargazers_count > b.stargazers_count) return -1;
-        if (a.stargazers_count < b.stargazers_count) return 1;
-        return 0;
-    });
-
-    return array;
-}
+	return array;
+};
